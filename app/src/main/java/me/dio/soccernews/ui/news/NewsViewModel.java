10a36bddfec1793524e1be78b87794d5968c378e
tfreitasf <tfreitasf@gmail.com>
2022-07-25 20:28:52 -0300

    refactoring

--- app/src/main/java/me/dio/soccernews/ui/news/NewsViewModel.java
+++ app/src/main/java/me/dio/soccernews/ui/news/NewsViewModel.java
@@ -1,16 +1,16 @@
-package me.dio.soccernews.ui.home;
+package me.dio.soccernews.ui.news;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class HomeViewModel extends ViewModel {
+public class NewsViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
 
-    public HomeViewModel() {
+    public NewsViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+        mText.setValue("This is news fragment");
     }
 
     public LiveData<String> getText() {
