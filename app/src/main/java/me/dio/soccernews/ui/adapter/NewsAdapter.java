df57c733bd048e0737137918fe304ab069c5989e
tfreitasf <tfreitasf@gmail.com>
2022-07-26 19:35:13 -0300

    implemented Intent to open the url

--- app/src/main/java/me/dio/soccernews/ui/adapter/NewsAdapter.java
+++ app/src/main/java/me/dio/soccernews/ui/adapter/NewsAdapter.java
@@ -1,5 +1,7 @@
 package me.dio.soccernews.ui.adapter;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
@@ -38,6 +40,13 @@ public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder> {
                 .load(news.getImage())
                 .fit()
                 .into(holder.binding.ivThumbnail);
+        holder.binding.btOpenLink.setOnClickListener(view -> {
+            Intent i = new Intent(Intent.ACTION_VIEW);
+            i.setData(Uri.parse(news.getLink()));
+            holder.itemView.getContext().startActivity(i);
+
+        });
+
 
     }
 
