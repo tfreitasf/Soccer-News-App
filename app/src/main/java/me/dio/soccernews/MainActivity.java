84f8098c744a5a531ac7edec61474af5ec6ad08f
tfreitasf <tfreitasf@gmail.com>
2022-07-27 21:32:58 -0300

    implemented favorite function

--- app/src/main/java/me/dio/soccernews/MainActivity.java
+++ app/src/main/java/me/dio/soccernews/MainActivity.java
@@ -2,19 +2,20 @@ package me.dio.soccernews;
 
 import android.os.Bundle;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+import androidx.room.Room;
 
+import me.dio.soccernews.data.local.AppDatabase;
 import me.dio.soccernews.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    private AppDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,12 +27,25 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+
+
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(R.id.navigation_home, R.id.navigation_dashboard).build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+
+        this.setupDb();
     }
 
+    private void setupDb() {
+        db = Room.databaseBuilder(this, AppDatabase.class, "soccer-news")
+                .allowMainThreadQueries()
+                .build();
+    }
+
+    public AppDatabase getDb() {
+        return db;
+    }
 }
--- app/src/main/java/me/dio/soccernews/ui/news/NewsFragment.java
+++ app/src/main/java/me/dio/soccernews/ui/news/NewsFragment.java
@@ -1,6 +1,5 @@
 package me.dio.soccernews.ui.news;
 
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,11 +8,9 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
-import androidx.loader.content.AsyncTaskLoader;
 import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.room.Room;
 
-import me.dio.soccernews.data.local.AppDatabase;
+import me.dio.soccernews.MainActivity;
 import me.dio.soccernews.databinding.FragmentNewsBinding;
 import me.dio.soccernews.ui.adapter.NewsAdapter;
 
@@ -21,7 +18,7 @@ public class NewsFragment extends Fragment {
 
     private FragmentNewsBinding binding;
 
-    private AppDatabase db;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         NewsViewModel newsViewModel = new ViewModelProvider(this).get(NewsViewModel.class);
@@ -30,16 +27,16 @@ public class NewsFragment extends Fragment {
         View root = binding.getRoot();
 
 
-        db = Room.databaseBuilder(getContext(), AppDatabase.class, "soccer-news")
-                .allowMainThreadQueries()
-                .build();
+
 
 
         binding.rvNews.setLayoutManager(new LinearLayoutManager(getContext()));
-
         newsViewModel.getNews().observe(getViewLifecycleOwner(), news -> {
             binding.rvNews.setAdapter(new NewsAdapter(news, updateNews -> {
-                db.newsDao().insert(updateNews);
+                MainActivity activity = (MainActivity) getActivity();
+                if (activity != null) {
+                    activity.getDb().newsDao().save(updateNews);
+                }
             }));
 
         });
@@ -51,4 +48,6 @@ public class NewsFragment extends Fragment {
         super.onDestroyView();
         binding = null;
     }
+
+
 }
